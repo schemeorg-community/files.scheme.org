b8e56f86e3dc39e2a7cd45aea9412e537f098a4c
Lassi Kortela <lassi@lassi.io>
2021-08-02 11:45:55 +0300

    Adopt common scheme.org page layout and CSS

--- upload.sh
+++ upload.sh
@@ -3,5 +3,7 @@ set -eu
 cd "$(dirname "$0")"
 ./index.sh
 set -x
+curl --location --fail --silent --show-error -o www/style.css \
+    https://www.staging.scheme.org/style.css
 rsync -crv www/ alpha.servers.scheme.org:/production/files/www/
 rsync -rv --size-only files/ alpha.servers.scheme.org:/production/files/www/
