2ce5a4c08a3b3e9e384f382ad5bde20a5acd5a1c
Lassi Kortela <lassi@lassi.io>
2023-03-03 11:31:42 +0200

    Download schemeorg.css from production server

--- upload.sh
+++ upload.sh
@@ -4,6 +4,6 @@ cd "$(dirname "$0")"
 ./index.sh
 set -x
 curl --location --fail --silent --show-error -o www/schemeorg.css \
-    https://www.staging.scheme.org/schemeorg.css
+    https://www.scheme.org/schemeorg.css
 rsync -crv www/ alpha.servers.scheme.org:/production/files/www/
 rsync -rv --size-only files/ alpha.servers.scheme.org:/production/files/www/
